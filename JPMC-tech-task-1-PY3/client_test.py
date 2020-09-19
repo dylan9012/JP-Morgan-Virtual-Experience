fdf5b50f2f850e0dd52272ab67761ca1ab37d291
dylan9012 <dylanbarker59@gmail.com>
2020-09-19 11:27:02 +0100

    Feedback on solution + Improvements

--- JPMC-tech-task-1-PY3/client_test.py
+++ JPMC-tech-task-1-PY3/client_test.py
@@ -32,17 +32,41 @@ class ClientTest(unittest.TestCase):
 
     """ ------------ Add more unit tests ------------ """
 
-    def test_getRatio_calculateRatio(self):
+    def test_getRatio_calculateRatio_ratioGreaterThanOne(self):
         quotes = [
-            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
-             'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 200.42, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 300.12, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
             {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
              'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
         ]
 
         price_a = (quotes[0]['top_ask']['price'] + quotes[0]['top_bid']['price']) / 2
         price_b = (quotes[1]['top_ask']['price'] + quotes[1]['top_bid']['price']) / 2
-        self.assertEqual(getRatio(price_a, price_b), price_a / price_b)
+        self.assertGreater(getRatio(price_a, price_b), 1)
+
+    def test_getRatio_calculateRatio_ratioLessThanOne(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 200.42, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 300.12, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+
+        price_a = (quotes[0]['top_ask']['price'] + quotes[0]['top_bid']['price']) / 2
+        price_b = (quotes[1]['top_ask']['price'] + quotes[1]['top_bid']['price']) / 2
+        self.assertLess(getRatio(price_a, price_b), 1)
+
+    def test_getRatio_calculateRatio_ratioEqualsOne(self):
+        quotes = [
+            {'top_ask': {'price': 100.01, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 100.01, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 100.01, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 100.01, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+
+        price_a = (quotes[0]['top_ask']['price'] + quotes[0]['top_bid']['price']) / 2
+        price_b = (quotes[1]['top_ask']['price'] + quotes[1]['top_bid']['price']) / 2
+        self.assertEqual(getRatio(price_a, price_b), 1)
 
     def test_getRatio_calculateRatio_priceAEqualsZero(self):
         quotes = [
@@ -66,8 +90,7 @@ class ClientTest(unittest.TestCase):
 
         price_a = (quotes[0]['top_ask']['price'] + quotes[0]['top_bid']['price']) / 2
         price_b = (quotes[1]['top_ask']['price'] + quotes[1]['top_bid']['price']) / 2
-        self.assertEqual(getRatio(price_a, price_b), None)
-
+        self.assertIsNone(getRatio(price_a, price_b))
 
 if __name__ == '__main__':
     unittest.main()
